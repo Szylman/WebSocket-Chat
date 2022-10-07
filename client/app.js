2a6749a2620bbb1aea89d306caa97636930d3ae2
Szylman96 <szymek2255@gmail.com>
2022-10-07 15:39:56 +0200

    all fix in app.js

--- client/app.js
+++ client/app.js
@@ -4,11 +4,11 @@ const messagesList = document.getElementById('messages-list');
 const addMessageForm = document.getElementById('add-messages-form');
 const userNameInput = document.getElementById('username');
 const messageContentInput = document.getElementById('message-content');
-const socket = io();
+//const socket = io();
 
 let userName = '';
 
-socket.on('message', ({ author, content }) => addMessage(author, content))
+//socket.on('message', ({ author, content }) => addMessage(author, content))
 
 loginForm.addEventListener('submit', login);
 addMessageForm.addEventListener('submit', sendMessage);
@@ -29,17 +29,16 @@ function sendMessage(event){
     if (messageContentInput === ''){
         return alert('Please add Message!')
     }else {
-        function addMessage(userName, messageContentInput){
-            socket.emit('message', { author: userName, content: messageContent })
-            messageContentInput.value = '';
-        }
+        addMessage(userName, messageContentInput.value);
+        //socket.emit('message', { author: userName, content: messageContent })
+        messageContentInput.value = '';
     }
 };
 
 function addMessage(author, content){
     const message = document.createElement('li');
-    message.classList.add('.message','.message--received');
-    author === userName ? message.classList.add('.message--self') : '';
+    message.classList.add('message','message--received');
+    author === userName ? message.classList.add('message--self') : '';
     message.innerHTML = `
         <h3 class="message__author">${userName === author ? 'You' : author}</h3>
         <div class="message__content">
